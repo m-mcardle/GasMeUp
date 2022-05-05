e2a6fa87d489f1786a36463cd636e11db37bba8f
Matt McArdle <matthew.j.mcardle@gmail.com>
2022-05-04 21:24:44 -0400

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  // Fetch data from the Node server
+  useEffect(() => {
+    fetch('http://tough-islands-feel-173-34-49-205.loca.lt/api')
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+      .catch((error) => console.log(error))
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
