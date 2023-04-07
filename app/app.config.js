4236f725c2f158012bd9c9500175b5cdc0d2f719
Matt McArdle <matthew.j.mcardle@gmail.com>
2023-04-06 22:54:11 -0400

    v1.0.20

--- app/app.config.js
+++ app/app.config.js
@@ -5,7 +5,7 @@ module.exports = {
   slug: 'gas-me-up',
   owner: 'mmcardle',
   originalFullName: '@mmcardle/GasMeUp',
-  version: '1.0.19',
+  version: '1.0.20',
   orientation: 'portrait',
   icon: './assets/car.png',
   splash: {
@@ -23,7 +23,7 @@ module.exports = {
   ios: {
     supportsTablet: false,
     bundleIdentifier: 'com.Virintus.GasMeUp',
-    buildNumber: '1.0.19',
+    buildNumber: '1.0.20',
     config: {
       googleMapsApiKey: process.env.GOOGLE_IOS_SDK_KEY,
       usesNonExemptEncryption: false,
@@ -41,7 +41,7 @@ module.exports = {
       backgroundColor: '#FFFFFF',
     },
     package: 'com.Virintus.GasMeUp',
-    versionCode: 470010018,
+    versionCode: 470010020,
   },
   web: {
     favicon: './assets/favicon.png',
