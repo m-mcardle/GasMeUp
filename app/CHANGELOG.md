e055a707c5facb53a030650a8a92fcd9d37be85f
Matt McArdle <matthew.j.mcardle@gmail.com>
2023-04-14 20:48:25 -0400

    v1.0.21

--- app/CHANGELOG.md
+++ app/CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to this project will be documented in this file. See [standa
 
 ### [1.0.21](https://github.com/m-mcardle/GasMeUp/compare/v1.0.20...v1.0.21) (2023-04-15)
 
+* Use new production server URL
+
 ### [1.0.20](https://github.com/m-mcardle/GasMeUp/compare/v1.0.19...v1.0.20) (2023-04-07)
 
 ### [1.0.19](https://github.com/m-mcardle/GasMeUp/compare/v1.0.18...v1.0.19) (2023-04-07)
--- app/app.config.js
+++ app/app.config.js
@@ -5,7 +5,7 @@ module.exports = {
   slug: 'gas-me-up',
   owner: 'mmcardle',
   originalFullName: '@mmcardle/GasMeUp',
-  version: '1.0.20',
+  version: '1.0.21',
   orientation: 'portrait',
   icon: './assets/car.png',
   splash: {
@@ -23,7 +23,7 @@ module.exports = {
   ios: {
     supportsTablet: false,
     bundleIdentifier: 'com.Virintus.GasMeUp',
-    buildNumber: '1.0.20',
+    buildNumber: '1.0.21',
     config: {
       googleMapsApiKey: process.env.GOOGLE_IOS_SDK_KEY,
       usesNonExemptEncryption: false,
@@ -41,7 +41,7 @@ module.exports = {
       backgroundColor: '#FFFFFF',
     },
     package: 'com.Virintus.GasMeUp',
-    versionCode: 470010020,
+    versionCode: 470010021,
   },
   web: {
     favicon: './assets/favicon.png',
