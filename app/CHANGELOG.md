5b4bc9df0ac212c8a940dad0fac1429713b541fe
m-mcardle <matthew.j.mcardle@gmail.com>
2025-03-15 17:21:05 -0400

    v1.1.2

--- app/CHANGELOG.md
+++ app/CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file. See [standa
 
 ### [1.1.2](https://github.com/m-mcardle/GasMeUp/compare/v1.1.1...v1.1.2) (2025-03-15)
 
+* Upgraded `@react-native-firebase` packages
+* Fixes Analytics going to staging Firebase
+* Fixes Remote Config not working
+
 ### [1.1.1](https://github.com/m-mcardle/GasMeUp/compare/v1.1.0...v1.1.1) (2025-03-13)
 
 * Fix minor layout bug on FriendInfoScreen table
--- app/app.config.js
+++ app/app.config.js
@@ -5,7 +5,7 @@ module.exports = {
   slug: 'gas-me-up',
   owner: 'mmcardle',
   originalFullName: '@mmcardle/GasMeUp',
-  version: '1.1.1',
+  version: '1.1.2',
   orientation: 'portrait',
   icon: './assets/car.png',
   splash: {
@@ -23,7 +23,7 @@ module.exports = {
   ios: {
     supportsTablet: false,
     bundleIdentifier: 'com.Virintus.GasMeUp',
-    buildNumber: '1.1.1',
+    buildNumber: '1.1.2',
     config: {
       googleMapsApiKey: process.env.GOOGLE_IOS_SDK_KEY,
       usesNonExemptEncryption: false,
@@ -43,7 +43,7 @@ module.exports = {
     },
     package: 'com.Virintus.GasMeUp',
     googleServicesFile: './google-services.json',
-    versionCode: 470010101,
+    versionCode: 470010102,
   },
   web: {
     favicon: './assets/favicon.png',
