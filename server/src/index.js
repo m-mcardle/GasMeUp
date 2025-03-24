3a9812abb3c71a9adad39c997b3800b0f60fdc82
m-mcardle <matthew.j.mcardle@gmail.com>
2025-03-24 11:23:53 -0400

    Actually import the function

--- server/src/index.js
+++ server/src/index.js
@@ -14,6 +14,7 @@ const {
 const {
   CanadianGasPriceRequest,
   AmericanGasPriceRequest,
+  ProvincialGasPricesRequest,
   CanadianGasPricesRequest,
   AmericanGasPricesRequest,
   WorldGasPricesRequest,
