166b077b02c9819d1a53f0347997036f255fdd06
m-mcardle <matthew.j.mcardle@gmail.com>
2025-03-24 11:06:52 -0400

    Fix `/cities` to `/province` on API for Gas Price API calls

--- server/src/queries/gasprice.js
+++ server/src/queries/gasprice.js
@@ -3,7 +3,7 @@ const url = 'https://canadian-gas-prices.p.rapidapi.com';
 function CanadianGasPriceRequest(province) {
   return {
     method: 'get',
-    url: encodeURI(`${url}/cities?province=${province}`),
+    url: encodeURI(`${url}/province?province=${province}`),
     headers: {
       'X-RapidAPI-Host': 'canadian-gas-prices.p.rapidapi.com',
       'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
